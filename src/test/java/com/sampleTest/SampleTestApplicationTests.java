86859664b7db1c6f456a7b7558f0e0fd7eb535db
Samir <attarsamir06@gmail.com>
2023-10-30 11:18:56 +0530

    Update

--- src/test/java/com/sampleTest/SampleTestApplicationTests.java
+++ src/test/java/com/sampleTest/SampleTestApplicationTests.java
@@ -1,21 +1,8 @@
 package com.sampleTest;
 
-import com.sampleTest.DAO.PersonDAO;
-import com.sampleTest.model.Person;
-import com.sampleTest.service.PersonService;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Optional;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import static org.mockito.Mockito.when;
-import org.mockito.junit.jupiter.MockitoExtension;
+
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -23,6 +10,6 @@ class SampleTestApplicationTests {
 
     @Test
     void name() {
-        System.out.println("sx");
+        System.out.println("AA");
     }
 }
