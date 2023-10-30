32f669b5cc416f9087f9960b6ec5b700d0076caf
Samir <attarsamir06@gmail.com>
2023-10-30 23:44:25 +0530

    Update with integartion testind

---
+++ src/test/java/com/sampleTest/IntegratinTesting/Controller/personControllerTest.java
@@ -0,0 +1,48 @@
+package com.sampleTest.IntegratinTesting.Controller;
+
+import com.sampleTest.DAO.PersonDAO;
+import com.sampleTest.model.Person;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.web.client.RestTemplate;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class personControllerTest {
+
+    @LocalServerPort
+    private int port;
+
+    private String baseUrl="http://localhost";
+
+    private static RestTemplate restTemplate;
+
+    @Autowired
+    private PersonDAO personDAO;
+
+
+    @BeforeAll
+    public static void init(){
+        restTemplate=new RestTemplate();
+    }
+
+    @BeforeEach
+    public void setUp(){
+        baseUrl=baseUrl.concat(":").concat(port+"").concat("/api/persons");
+    }
+
+    @Test
+    public void testAddProduct(){
+        Person person = new Person(10, "John", "nyc");
+        Person respo = restTemplate.postForObject(baseUrl, person, Person.class);
+        assertEquals("John", respo);
+        assertEquals(10,personDAO.findAll().size());
+    }
+
+
+}
